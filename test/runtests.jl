34c69456ff13833c47436494e1d0e94ee90382ca
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-03-08 11:53:09 +0530

    Test project quality with Aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 module TestMicroCollections
 using Test
 
+module ProjectQuality
+    using Aqua
+    using Test
+    import MicroCollections
+    @testset "project quality" begin
+        Aqua.test_all(MicroCollections, ambiguities=false)
+    end
+end
+
 @testset "$file" for file in sort([
     file for file in readdir(@__DIR__) if match(r"^test_.*\.jl$", file) !== nothing
 ])
